fd0b7059dbfdedaa5bf82f48c90d6bff04993306
Faye Buchanan <zirilyth@gmail.com>
2025-12-10 15:21:53 +0000

    Made a test fail

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -24,6 +24,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Something that wont be there');
   });
 });
